657489c0fceb4606348ebcac157f97615a0733d7
Aleksandra <ajieksandra.93@mail.ru>
2024-10-15 15:06:20 +0500

    Добавлена возможность выбора тэга из имеющихся

--- Cataas.py
+++ Cataas.py
@@ -1,9 +1,11 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from pygame.examples.moveit import load_image
+
+allowed_tags = ['sleep', 'jump', 'fight', 'black', 'white', 'bengal', 'siamese', 'cute', 'red']
 
 def load_image(url):
     try:
@@ -18,7 +20,7 @@ def load_image(url):
         return None
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f'https://cataas.com/cat/{tag}' if tag else 'https://cataas.com/cat'
     img = load_image(url_tag)
     if img:
@@ -30,18 +32,12 @@ def open_new_window():
         label.config(image=img)
         label.image = img
 
-def quit():
+def exit():
     window.destroy()
 
 window = Tk()
 window.title('Cats')
-window.geometry('200x100')
-
-tag_entry = Entry(window)
-tag_entry.pack()
-
-load_btn = Button(window, text='Загрузить по тегу', command=open_new_window)
-load_btn.pack()
+window.geometry('200x150')
 
 menubar = Menu(window)
 window.config(menu=menubar)
@@ -49,6 +45,15 @@ file_menu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label='Файл', menu=file_menu)
 file_menu.add_command(label='Загрузить фото', command=open_new_window)
 file_menu.add_separator()
-file_menu.add_command(label='Выход', command=quit)
+file_menu.add_command(label='Выход', command=exit)
+
+tag_label = Label(text='Выберите тэг')
+tag_label.pack(pady=10)
+
+tag_combobox = ttk.Combobox(values=allowed_tags)
+tag_combobox.pack(pady=10)
+
+load_btn = Button(window, text='Загрузить по тегу', command=open_new_window)
+load_btn.pack()
 
 window.mainloop()
